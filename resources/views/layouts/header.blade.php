1355a16f5cab18dda5441a87607a4248ce0d035c
jlopez1423 <jlopez46@ucsc.edu>
2018-01-19 21:45:58 -0800

    Edit: Tooling and css path fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="{{base_path('public/css/main.css')}}">
+    <link href="/css/main.css" rel="stylesheet" type="text/css"/>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
