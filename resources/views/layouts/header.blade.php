e03f415c9b07bbbfe0b88a8f9eaabd95ce65c20b
Jose <jalopez1423@gmail.com>
2018-05-23 20:15:48 -0700

    front end edits.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    <link href="{{ base_path('/css/styles.css') }}/public/css/styles.css" rel="stylesheet" type="text/css"/>
+    <link  rel="stylesheet" href="{{urlGenerator('/css/styles.css')}}">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
